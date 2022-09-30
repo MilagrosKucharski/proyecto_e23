8c3a7b4562e01491aaf4a2da8a3a56fbe2ca7c31
Your NameFranco <you@example.comfranco.malugani@correo.ucu.edu.uy>
2022-09-30 19:58:01 -0300

    constructor EmployerRequest

--- src/Library/EmployerRequest.cs
+++ src/Library/EmployerRequest.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections;
 namespace Library{
     public class EmployerRequest{
+
+        public EmployerRequest(string request){
+
+            this.Request = request;
+        }
         public ArrayList AwaitingRequests = new ArrayList();
         
         public ArrayList DeclainedRequests = new ArrayList();
 
         public string Request { get; set;}
 
-        public void AddRequest (Request){
+        public void AddRequest(Request){
 
             this.AwaitingRequests.Add(Request);
         }
